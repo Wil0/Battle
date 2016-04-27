8cab7702591a3fa4b8211971ed46f5a97ed5c5f2
emilio <emsmnndz@gmail.com>
2016-04-27 15:55:10 +0100

    Challenge#26: making controllers skinny

--- app.rb
+++ app.rb
@@ -11,24 +11,27 @@ class Battle < Sinatra::Base
   end
 
   post "/names" do
-    $player1 = Player.new(params[:player1])
-    $player2 = Player.new(params[:player2])
-    $game = Game.new($player1, $player2)
+    $game = Game.new(Player.new(params[:player1]), Player.new(params[:player2]))
+    # $player1 = Player.new(params[:player1])
+    # $player2 = Player.new(params[:player2])
+    # $game = Game.new($player1, $player2)
     redirect '/play'
   end
 
   get '/play' do
-    @player1 = $player1.name
-    @player2 = $player2.name
-    @player2_points = $player2.points
+    # @player1 = $game.player1.name
+    # @player2 = $game.player2.name
+    # @player2_points = $game.player2.points
+    @game = $game
     erb(:play)
   end
 
   get '/attack' do
-    @player1 = $player1.name
-    @player2 = $player2.name
-    $game.attack($player2)
-    @player2_points = $player2.points
+    # @player1 = $game.player1.name
+    # @player2 = $game.player2.name
+    @game = $game
+    @game.attack(@game.player2)
+    # @player2_points = $game.player2.points
     erb(:attack)
   end
 
