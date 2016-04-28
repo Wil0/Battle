c55a96d2b7d7a4474c46bdc593111d3cce5555bf
emilio <emsmnndz@gmail.com>
2016-04-28 09:53:26 +0100

    feature tested new text when showing palyer HP

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -2,7 +2,7 @@ feature 'Attack' do
   scenario 'Attack player 2' do
     sign_in_and_play
     click_button('Attack!')
-    expect(page).to have_content('Matt has been hit, the sucker')
+    expect(page).to have_content("BOOM! Emilio scores a hit!")
   end
 
   scenario ' Reduce HP by 10' do
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,11 +1,11 @@
 feature 'Viewing hit points' do
   scenario 'show player1 hit points' do
     sign_in_and_play
-    expect(page).to have_content("Emilio hit points: 100")
+    expect(page).to have_content("Today's battle: Emilio vs Matt Emilio's hit points: 100 Matt's hit points: 100 Emilio is up first! Let the games begin!")
   end
 
   scenario 'show player2 hit points' do
     sign_in_and_play
-    expect(page).to have_content("Matt hit points: 100")
+    expect(page).to have_content("Today's battle: Emilio vs Matt Emilio's hit points: 100 Matt's hit points: 100 Emilio is up first! Let the games begin!")
   end
 end
