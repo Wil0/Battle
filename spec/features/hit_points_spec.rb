8a3b3c129e1f3c31020f3fcc85c20846b6acaae8
emilio <emsmnndz@gmail.com>
2016-04-28 09:55:11 +0100

    refactored feature tests

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,11 +1,17 @@
 feature 'Viewing hit points' do
   scenario 'show player1 hit points' do
     sign_in_and_play
-    expect(page).to have_content("Today's battle: Emilio vs Matt Emilio's hit points: 100 Matt's hit points: 100 Emilio is up first! Let the games begin!")
+    expect(page).to have_content("Today's battle:
+    Emilio vs Matt Emilio's hit points:
+    100 Matt's hit points: 100 Emilio is up first!
+    Let the games begin!")
   end
 
   scenario 'show player2 hit points' do
     sign_in_and_play
-    expect(page).to have_content("Today's battle: Emilio vs Matt Emilio's hit points: 100 Matt's hit points: 100 Emilio is up first! Let the games begin!")
+    expect(page).to have_content("Today's battle:
+    Emilio vs Matt Emilio's hit points:
+    100 Matt's hit points: 100 Emilio is up first!
+    Let the games begin!")
   end
 end
