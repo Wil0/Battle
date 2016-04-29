93daeda139ddad3cdcc818279323f02edec6daa6
emilio <emsmnndz@gmail.com>
2016-04-29 12:21:04 +0100

    Added restart button when a player loses

--- spec/features/lose_game_spec.rb
+++ spec/features/lose_game_spec.rb
@@ -4,4 +4,11 @@ feature 'shows game over screen' do
     19.times { click_button('Attack!') }
     expect(page).to have_content("Matt loses!")
   end
+
+  scenario 'it allows player to restart the game' do
+    sign_in_and_play
+    19.times { click_button('Attack!') }
+    click_button('Restart')
+    expect(page).to have_content("Welcome to the Arena!")
+  end
 end
